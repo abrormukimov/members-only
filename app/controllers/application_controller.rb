7d5af7aade48ba7c59d4443b5797298edaf7877e
abrormukimov <abrormm34@gmail.com>
2020-07-18 18:22:57 +0500

    Add bulma style to views, create helper methods, add member model and some partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def sign_up_params
+    params.require(:member).permit(:name, :username, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:member).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_120719) do
+ActiveRecord::Schema.define(version: 2020_07_18_130211) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,14 +20,18 @@ ActiveRecord::Schema.define(version: 2020_07_18_120719) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_members_on_username", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "member_id"
   end
 
 end
