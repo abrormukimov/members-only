dbe76e1948898caf1f7ffec93f336be660857e75
abrormukimov <abrormm34@gmail.com>
2020-07-19 22:47:07 +0500

    Move logic to the view helper and change router path access

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,29 @@
 module PostsHelper
+  def timeline_member_gravatar(member)
+    if member_signed_in?
+      gravatar_image_tag(member.email, size: 64, alt: member.name)
+    end
+  end
+
+  def timeline_member_name(post)
+    if member_signed_in?
+      post.member.name
+    end
+  end
+
+  def timeline_member_username(post)
+    if member_signed_in?
+      post.member.username
+    end
+  end
+
+  def timeline_member_post(post)
+    post.post
+  end
+
+  def member_profile_index
+    if member_signed_in?
+      render 'profile'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :members, :controllers => { registrations: 'registrations'}
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
+  resources :posts, only: [:new, :create, :index]
+  resources :members, only: [:index, :edit, :show, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
